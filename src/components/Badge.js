0c6ad141472b8f3e69d8822ea23956c36b2df5c6
Camilo Rodriguez <camilorc1129@gmail.com>
2020-04-01 23:05:47 +1100

    creat first component Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import animeLogo from '../images/logo.png';
+import './styles/Badge.css';
 
 class Badge extends React.Component {
     render(){
         return(
-            <h1>Badge</h1>
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={animeLogo} alt="logo BadgeAnime"/>
+                </div>
+
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar" alt="Avatar"/>
+        <h1> {this.props.firstName} <br />  {this.props.lastName} </h1>
+                </div>
+
+                <div className="Badge__section-info">
+                    <h3>Dragon Ball Z</h3>
+                    <div>
+                        <p>Power fight: {this.props.powerFight}</p>
+                        <p>Special Trait: {this.props.specialTrait}</p>
+                    </div>
+                </div>
+
+                <div className="Badge__footer">
+                    #badgeAnime
+                </div>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Badge from './components/Badge.js';
 //import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root')
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(<Badge  
+                    firstName="Son" 
+                    lastName="Camilo"
+                    powerFight = "1000/1500"
+                    specialTrait = "Saiyan"
+                    />, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
